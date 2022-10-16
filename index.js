9c93f0764be306edfd6baeadffb2c49b962423c2
Jeremy Lloyd <jeremylloyd3@gmail.com>
2022-10-16 13:47:37 +1100

    Added reset, decimal and delete functionality

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ input = null;
 output = null;
 const operators = ["div", "plus", "minus", "mul"];
 operator = null;
+decimalPlace = 0;
+let outputDp;
 
 const body = document.querySelector("body");
 const themeSwitch = document.querySelector(".switch");
 const themeSwitchSlider = document.querySelector(".switch__slider");
 const calcOutput = document.querySelector(".calc__output");
 
-const commaFormatter = Intl.NumberFormat("en");
-const scientificFormatter = Intl.NumberFormat("en", { notation: "scientific" });
-
+reset();
 updateConsole(output);
 
 themeSwitch.addEventListener("click", toggleTheme);
-document.querySelector(".calc__button--reset");
-document.querySelector(".calc__button--del");
+document.querySelector(".calc__button--reset").addEventListener("click", reset);
+document
+  .querySelector(".calc__button--del")
+  .addEventListener("click", processDelete);
 for (i = 0; i < 10; i++) {
   key = document.querySelector(`.calc__button--${i}`);
   key.number = i;
@@ -30,6 +32,9 @@ for (i = 0; i < operators.length; i++) {
 document
   .querySelector(".calc__button--equals")
   .addEventListener("click", processEquals);
+document
+  .querySelector(".calc__button--decimal")
+  .addEventListener("click", processDecimal);
 
 function toggleTheme() {
   oldTheme = theme;
@@ -38,8 +43,11 @@ function toggleTheme() {
   themeSwitchSlider.style.transform = `translateX(${24 * (theme - 1)}px)`;
 }
 
-function updateConsole(number) {
+function updateConsole(number, dp = 0) {
   // Print a number to the output screen
+  let commaFormatter = Intl.NumberFormat("en", {
+    maximumFractionDigits: Math.max(15, dp),
+  });
   consoleStr = commaFormatter.format(number);
   calcOutput.innerHTML = `${consoleStr}`;
 }
@@ -47,13 +55,20 @@ function updateConsole(number) {
 function processNumber() {
   if (!operator) {
     output = null;
+    outputDp = 0;
   }
-  if (!input) {
+  if (!input && !decimalPlace) {
     input = this.number;
   } else {
-    input = Number(String(input) + String(this.number));
+    if (decimalPlace) {
+      term = this.number / 10 ** decimalPlace;
+      input += term;
+      decimalPlace += 1;
+    } else {
+      input = input * 10 + this.number;
+    }
   }
-  updateConsole(input);
+  updateConsole(input, decimalPlace - 1);
 }
 
 function processOperator() {
@@ -66,7 +81,9 @@ function processOperator() {
   }
 
   operator = this.operator;
+  outputDp = decimalPlace;
   input = null;
+  decimalPlace = 0;
 }
 
 function processEquals() {
@@ -76,10 +93,24 @@ function processEquals() {
     } else {
       output = input;
     }
+    outputDp = decimalPlace;
     updateConsole(output);
   }
   input = null;
   operator = null;
+  decimalPlace = 0;
+}
+
+function processDecimal() {
+  decimalPlace = 1;
+}
+
+function reset() {
+  input = null;
+  output = null;
+  operator = null;
+  decimalPlace = 0;
+  updateConsole(output);
 }
 
 function calculate(a, b, op) {
@@ -91,5 +122,25 @@ function calculate(a, b, op) {
     return a * b;
   } else if (op === "div") {
     return a / b;
+  }
+}
+
+function decimalPlacesCount(number) {
+  const numberStr = String(number);
+  decimalIndex = numberStr.indexOf(".");
+  if (decimalIndex === -1) {
+    return 0;
+  } else {
+    return numberStr.slice(decimalIndex + 1).length;
+  }
+}
+
+function processDelete() {
+  if (input) {
+    input = Number(String(input).slice(0, -1));
+    decimalPlace = Math.max(0, decimalPlace - 1);
+    updateConsole(input, decimalPlace - 1);
+  } else {
+    reset();
   }
 }
